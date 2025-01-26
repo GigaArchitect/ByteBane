59c84a80cff8b9175c7eddb320a420453768770c
GigaArchitect <nsar90554046@gmail.com>
2025-01-27 00:45:24 +0200

    fix(main.c) : fix memory leaks and add checking for allocations

--- main.c
+++ main.c
@@ -31,6 +31,8 @@ int encrypt_file(char * file_path, AES_KEY key){
         char * copied_line = strdup(buffer_line);
         linked_list = g_list_append(linked_list, copied_line);
     }
+    free(buffer_line);
+    fclose(buffer);
 
     for (GList * l = linked_list; l != NULL; l = l->next){
         char * line = strdup((char *) l->data);
@@ -46,7 +48,6 @@ int encrypt_file(char * file_path, AES_KEY key){
         free(l->data);
         l->data = line;
     }
-    fclose(buffer);
 
     const char* file_extension = ".enc";
     char* new_file_path = malloc(strlen(file_path) + strlen(file_extension) + 1);
@@ -59,6 +60,7 @@ int encrypt_file(char * file_path, AES_KEY key){
     for(GList * l = linked_list; l != NULL; l = l->next){
         fwrite(l->data, strlen(l->data), 1, buffer);
     }
+    free(new_file_path);
     fclose(buffer);
 
     g_list_free(linked_list);
@@ -83,29 +85,59 @@ void* encrypt_file_thread(void *arg) {
 
 int main() {
     char *key = getenv("BYTE_BANE_KEY");
+    if (!key) {
+        perror("Environment variable BYTE_BANE_KEY not set");
+        return 1;
+    }
+
     AES_KEY aes_key;
-    AES_set_encrypt_key(key, 128, &aes_key);
+    if (AES_set_encrypt_key((unsigned char *)key, 128, &aes_key) != 0) {
+        perror("Unable to set encryption key");
+        return 1;
+    }
 
     DIR *current_dir;
     struct dirent *dir;
-    
+
     GArray *threads = g_array_new(FALSE, FALSE, sizeof(pthread_t));
-    
+
     current_dir = opendir(".");
     if (current_dir) {
         while ((dir = readdir(current_dir)) != NULL) {
-            if (strcmp(dir->d_name, ".") == 0 || 
-                strcmp(dir->d_name, "..") == 0 || 
+            if (strcmp(dir->d_name, ".") == 0 ||
+                strcmp(dir->d_name, "..") == 0 ||
                 strcmp(dir->d_name, "main") == 0) {
                 continue;
             }
 
             thread_args_t *args = malloc(sizeof(thread_args_t));
+            if (!args) {
+                perror("Unable to allocate memory for thread arguments");
+                closedir(current_dir);
+                g_array_free(threads, TRUE);
+                return 1;
+            }
+
             args->filename = strdup(dir->d_name);
+            if (!args->filename) {
+                perror("Unable to duplicate filename");
+                free(args);
+                closedir(current_dir);
+                g_array_free(threads, TRUE);
+                return 1;
+            }
+
             args->key = aes_key;
 
             pthread_t thread;
-            pthread_create(&thread, NULL, encrypt_file_thread, args);
+            if (pthread_create(&thread, NULL, encrypt_file_thread, args) != 0) {
+                perror("Unable to create thread");
+                free(args->filename);
+                free(args);
+                closedir(current_dir);
+                g_array_free(threads, TRUE);
+                return 1;
+            }
             g_array_append_val(threads, thread);
         }
         closedir(current_dir);
@@ -113,7 +145,11 @@ int main() {
 
     for (guint i = 0; i < threads->len; i++) {
         pthread_t thread = g_array_index(threads, pthread_t, i);
-        pthread_join(thread, NULL);
+        if (pthread_join(thread, NULL) != 0) {
+            perror("Unable to join thread");
+            g_array_free(threads, TRUE);
+            return 1;
+        }
     }
 
     g_array_free(threads, TRUE);
